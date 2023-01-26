0b1745720cdf1780d5c1f22c383eac399535c32d
Minji Seo <63698668+ireneeming@users.noreply.github.com>
2023-01-26 17:07:22 +0900

    ⚡️ : main.dart 파일의 기본적인 이해

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,56 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Material은 구글이 만든 스타일 도구이며, 외부 패키지 불러올 때 import 해서 갖고온다.
 
 void main() {
+  // main 함수, 플러터 프로젝트 실행할 때, 이 부분부터 실행.
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // 위에서 실행 한 MyApp을 정의하는 코드. extend 구문을 통해 상태가 없는 위젯(state less widget)을 상속하여 MyApp 클래스를 하나의 위젯처럼 만듬.
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // 모든 위젯은 공통적으로 Widget build(BuldContext context) {} 메서드를 가지고 있음. 부모 위젯인 statelessWidget 혹은 statefulWidget의 메서드 이므로 @override 가 붙음.
+    // BuildContext는 모든 위젯이 가지는 고유의 아이디값이라고 이해하면 됨.
     return MaterialApp(
+      // Widget build() 의 리턴으로 MaterialApp() 위젯이 반환되고 있음. MaterialApp 은 말 그대로 Material 스타일의 App 위젯이며, 앱이라는 가장 큰 위젯이 시작되는 것.
+
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        // 내 앱의 테마
+        // primarySwatch 의 Colors.blue -> Colors.green 으로 수정 후 저장하면 바로 반영되는데
+        // 이를 Hot-Reload 라고 함. 상단 위에 번개모양
+        // 가끔 저장해도 Hot-Reload 기능이 적용되지 않는 경우에는 다시 빌드해야 함.
+        // 그럴 때는 Hot-Restart 번개모양 오른쪽 버튼 누르면 됨.
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+          title:
+              'Flutter Demo Home Page'), // MaterialApp 의 home 프로퍼티. 홈페이지, 즉 첫 화면을 나타내는 위젯이 들어가야 함. home 프로퍼티에 들어간 위젯은 MyHomePage 위젯으로, StatefulWidget임.
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
+  final String title; // final은 클래스 내 선언되는 플로퍼티가 생성할 때 처음 설정되고 변경되지 않을 값일 때 사용.
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // title 이라는 프로퍼티를 함께 선언해주고, 내부에 this.title로 넣어줌. this는 해당 객체 내 값에 접근하기 위한 키워드.
+  // required 는 꼭 선언해줘야 하는 값이라는 뜻. title의 기본값 선언 안해줬지 때믄에 required 가 필요함.
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // Scaffold() 위젯. 앱 화면이 기본적으로 갖추고 있는 기능들을 선언해 놓은 위젯.
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          title,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: const Center(
+        child: Text('Hello, Flutter!!!!'),
+      ),
     );
   }
 }
